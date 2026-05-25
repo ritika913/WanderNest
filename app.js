643812e5e6a25010066ecc8ce5ded881d7e96d88
ritika913 <ritika2013.r@gmail.com>
2026-05-25 11:16:47 +0530

    Updated app configuration and dependencies

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 const session=require("express-session");
-const MongoStore=require("connect-mongo")
+const MongoStore = require("connect-mongo").default;
 const flash=require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
@@ -47,7 +47,7 @@ const store=MongoStore.create({
     touchAfter:24*3600,
 });
 
-store.on("error",()=>{
+store.on("error",(err)=>{
     console.log("ERROR in MONGO SESSION STORE",err);
 });
 
@@ -73,6 +73,11 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currUser = req.user;
+    next();
+});
 
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");    
